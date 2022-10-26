b04631c9537bf1ad8e39150eb575a86ae7b71f8e
Brian LEE <93697790+CodyMan0@users.noreply.github.com>
2022-10-26 23:16:27 +0900

    Fix : 불필요한 코드 리코일 삭제

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
-import { RecoilRoot } from 'recoil';
 import Router from './Router';
 import GlobalStyle from './styles/GlobalStyle';
 import variables from './styles/variable';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <RecoilRoot>
-    <ThemeProvider theme={{ style: variables }}>
-      <GlobalStyle />
-      <Router />
-    </ThemeProvider>
-  </RecoilRoot>
+  <ThemeProvider theme={{ style: variables }}>
+    <GlobalStyle />
+    <Router />
+  </ThemeProvider>
 );
