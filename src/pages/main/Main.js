f283b3edc5f780390241da86f827f4f7dc48d343
Youngjun Choi <juni2004c@gmail.com>
2022-10-27 14:56:10 +0900

    setting: eslint airbnb style 적용

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
+var test="1213";;;;;;;;;;;;;;;;;;;;
+
+
+
+
+console.log("ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss")
+
+
 const Main = () => {
   return <div>Main</div>;
 };
